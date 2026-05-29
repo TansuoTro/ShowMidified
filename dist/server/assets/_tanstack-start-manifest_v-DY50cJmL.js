d14334520e8610e482fb8fe3962d7308524e0e2b
CARIERX <CARIERX01@ITP.AC.CN>
2026-05-29 17:35:22 +0800

    chore: add .gitignore, remove tracked build artifacts

--- dist/server/assets/_tanstack-start-manifest_v-DY50cJmL.js
+++
@@ -1,4 +0,0 @@
-const tsrStartManifest = () => ({ routes: { __root__: { filePath: "H:/Tools/WebApp/ShowMidified/midi-live/src/routes/__root.tsx", children: ["/"], assets: [{ tag: "link", attrs: { rel: "stylesheet", href: "/assets/index-CIoet5Iw.css", type: "text/css" } }], preloads: ["/assets/index-Cak6grbM.js"] }, "/": { filePath: "H:/Tools/WebApp/ShowMidified/midi-live/src/routes/index.tsx", children: void 0, assets: [], preloads: ["/assets/index-CLP9RQTR.js"] } }, clientEntry: "/assets/index-Cak6grbM.js" });
-export {
-  tsrStartManifest
-};
